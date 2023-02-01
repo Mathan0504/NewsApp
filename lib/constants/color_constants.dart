2729ec2301cbac7881686185e6bc2164a13f4e57
Mathan <Mathansiva0504@gmail.com>
2023-02-01 20:35:03 +0530

    first commit

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color orangeWeb = Color.fromARGB(255, 70, 209, 82);
+  static const Color white = Color.fromARGB(255, 243, 239, 239);
+  static const Color lightGrey = Color(0xFFd2d7df);
+  static const Color burgundy = Color.fromARGB(255, 128, 145, 150);
+}
---
+++ lib/constants/newsApi_constants.dart
@@ -0,0 +1,21 @@
+class NewsApiConstants {
+  NewsApiConstants._();
+  static const newsApiKey = '60f1e369450c457eb4402678c2225a28';
+  static const mediaStackApi = '369446dcca7d94d8f8863a58c809a483';
+  static const articles = 'articles';
+  static const mediaStackData = 'data';
+  static const id = 'id';
+  static const name = 'name';
+  static const source = 'source';
+  static const author = 'author';
+  static const title = 'title';
+  static const image = 'image';
+  static const description = 'description';
+  static const url = 'url';
+  static const urlToImage = 'urlToImage';
+  static const publishedAt = 'publishedAt';
+  static const content = 'content';
+  static const language = 'language';
+  static const category = 'category';
+  static const country = 'country';
+}
